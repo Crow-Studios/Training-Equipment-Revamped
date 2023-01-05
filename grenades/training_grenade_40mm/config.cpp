5cf6230acb2950f77dcf7a03f3396a07ec8a0794
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2023-01-05 19:22:52 +0000

    40mm compatibility

---
+++ grenades/training_grenade_40mm/config.cpp
@@ -0,0 +1,50 @@
+class cfgPatches
+{
+	class training_40mm_common
+	{
+		units[] =
+		{
+			""
+		};
+		weapons[] =
+		{
+			""
+		};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"training_grenade_common"};
+	};
+};
+
+#include "config\cfgTrainingGrenades.hpp"
+
+class cfgAmmo
+{
+	class training_40mm_base;
+
+	class training_40mm_he_base: training_40mm_base {};
+};
+
+class cfgMagazines 
+{
+	class training_40mm_mag_base;
+
+	class training_40mm_mag_he: training_40mm_mag_base
+	{
+		scope = 2;
+		displayName="Training 40mm";
+		displayNameShort="40mm";
+		ammo="training_40mm_he_base";
+		picture = "\training_core\crow_studios_co.paa";
+	};
+};
+
+class CfgMagazineWells
+{
+	class CBA_40mm_M203
+	{
+		training_mag_compat_40mm[]=
+		{
+			"training_40mm_mag_he"
+		};
+	};
+};
--- grenades/training_grenade_common/config/cfgAmmo.hpp
+++ grenades/training_grenade_common/config/cfgAmmo.hpp
@@ -44,4 +44,46 @@ class cfgAmmo
 
     class smokeShellBlue;
 	class training_grenade_smoke_base: SmokeShellBlue {};
+
+	class G_40mm_HE;
+	class training_40mm_base : G_40mm_HE
+	{
+		// ACE_damageType = "";
+		// ace_frag_enabled = 0;
+
+		scope = 2;
+		hit=0;
+		indirectHit=0; 
+		indirectHitRange=0;
+		dangerRadiusHit=0;
+		suppressionRadiusHit=0;
+
+		CraterEffects="";
+		explosionEffects="";
+		explosive=0;
+		explosionTime = 2;
+		soundHit[]=
+		{
+			"",
+			0,
+			0
+		};
+		cost=100;
+		whistleDist=0;
+
+		class CamShakeExplode
+		{
+			power=2;
+			duration=1;
+			frequency=10;
+			distance=1;
+		};
+		class CamShakeHit
+		{
+			power=5;
+			duration=3;
+			frequency=20;
+			distance=2;
+		};
+	};
 };
